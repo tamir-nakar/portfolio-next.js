319d06246575dc63fff97396f5ddb552f9e78471
tamir-nakar <nakar.tamir@gmail.com>
2024-02-26 01:13:18 +0200

    fix(mdx files were flawed):

--- app/page.tsx
+++ app/page.tsx
@@ -36,13 +36,6 @@ export default function Home() {
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
           Senior Full-Stack developer{" "}
-          {/* <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers. */}
         </h2>
       </div>
     </div>
