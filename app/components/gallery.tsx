da3834c3d3b9698cac45391abff8ca06711e8308
tamirn <nakar.tamir@gmail.com>
2024-04-19 02:01:48 +0300

    feat(Gallery component): add configurations and minor refactor

--- app/components/gallery.tsx
+++ app/components/gallery.tsx
@@ -1,22 +1,27 @@
 import React, { useState } from "react";
 import Grid from "@mui/material/Grid"; // Importing Grid from MUI
-import Dialog from "@mui/material/Dialog"; // Importing Dialog from MUI
+import { Dialog, DialogTitle } from "@mui/material"; // Importing Dialog from MUI
 import IconButton from "@mui/material/IconButton"; // Importing IconButton from MUI
 import CloseIcon from "@mui/icons-material/Close"; // Importing CloseIcon from MUI icons
 import YouTubeIcon from "@mui/icons-material/YouTube";
-
+import ZoomInSharpIcon from "@mui/icons-material/ZoomInSharp";
 export interface MediaDescriptor {
   description?: string;
   src: string;
   type: Type;
 }
 
+export interface GalleryConfig {
+  iconColor?: string;
+}
+
 type Type = "video" | "image";
 interface GalleryProps {
   images: MediaDescriptor[];
+  config: GalleryConfig;
 }
 
-const Gallery: React.FC<GalleryProps> = ({ images }) => {
+const Gallery: React.FC<GalleryProps> = ({ images, config }) => {
   const [open, setOpen] = useState<boolean>(false);
   const [selectedImage, setSelectedImage] = useState<MediaDescriptor | null>(
     null
@@ -34,7 +39,7 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
 
   return (
     <div>
-      <Grid container spacing={1}>
+      <Grid container rowSpacing={0} columnSpacing={1}>
         {images.slice(0, 5).map((image, index) => {
           let gridProps;
           switch (images.length) {
@@ -48,22 +53,21 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
               gridProps =
                 index === 0
                   ? { xs: 12, sm: 12, md: 12 }
-                  : { xs: 6, sm: 6, md: 12 / (images.length - 1) };
+                  : { xs: 12, sm: 6, md: 12 / (images.length - 1) };
               break;
           }
           return (
             <Grid item {...gridProps} key={index}>
-
               {image.type === "video" ? (
                 <React.Fragment>
-                <iframe
-                width="100%"
-                height="315"
-                src={image.src}
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              ></iframe>
-                {/* <YouTubeIcon
+                  <iframe
+                    width="100%"
+                    height="315"
+                    src={image.src}
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  ></iframe>
+                  {/* <YouTubeIcon
                   sx={{
                     position: "relative",
                     top: -60,
@@ -74,12 +78,23 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
                 /> */}
                 </React.Fragment>
               ) : (
-                <img
-                  src={image.src}
-                  alt={`Image ${image.description || `image: ${index + 1}`}`}
-                  style={{ width: "100%", height: "auto", cursor: "pointer" }}
-                  onClick={() => handleClickOpen(image)}
-                />
+                <React.Fragment>
+                  <img
+                    src={image.src}
+                    alt={`Image ${image.description || `image: ${index + 1}`}`}
+                    style={{ width: "100%", height: "auto", cursor: "pointer" }}
+                    onClick={() => handleClickOpen(image)}
+                  />
+                  <ZoomInSharpIcon
+                    sx={{
+                      position: "relative",
+                      top: -60,
+                      right: -5,
+                      fontSize: 30,
+                      color: config.iconColor || "#f3f3f3",
+                    }}
+                  />
+                </React.Fragment>
               )}
             </Grid>
           );
@@ -93,6 +108,8 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
         maxWidth="md"
         fullWidth
       >
+        <DialogTitle id="form-dialog-title" style={{ fontFamily: 'Roboto, sans-serif' }}>{selectedImage?.description ?? ''}</DialogTitle>
+
         <IconButton
           aria-label="close"
           onClick={handleClose}
